4fcfaf1129708006089ec871f2c6e48269cc9d9a
Filip Madunicky <yelidric@gmail.com>
2025-12-31 18:37:45 +0100

    Add axios interceptor to handle 401 errors and redirect to login

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { CssBaseline } from "@mui/material";
+import axios from "axios";
 import App from "./App";
 import "./index.css";
 import NotifificationProvider from "./contexts/notificationContext";
 import AuthContextProvider from "./contexts/authContext";
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      setTimeout(() => {
+        window.localStorage.removeItem("loggedAppUser");
+        window.location.href = "/";
+      }, 5000);
+    }
+    return Promise.reject(error);
+  }
+);
 
 const queryClient = new QueryClient();
 
